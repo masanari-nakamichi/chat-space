162c0ace143c9c8f3d89d8606095901048a1491f
masanari-nakamichi <shirohana2627@gmail.com>
2020-01-31 17:54:10 +0900

    add chat function

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,7 +1,7 @@
 class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -24,14 +24,14 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
-  #
+  process resize_to_fit: [800, 800]
   # def scale(width, height)
   #   # do something
   # end
-  process resize_to_fit: [800, 800]
+  
   # Create different versions of your uploaded files:
   # version :thumb do
-  #   process resize_to_fit: [50, 50]
+  #   process resize_to_fit: [50, 50] 
   # end
 
   # Add a white list of extensions which are allowed to be uploaded.
