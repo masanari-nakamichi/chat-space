3c94dcad1074bf4fd7ea1157a9dfec3957f4acb8
masanari-nakamichi <shirohana2627@gmail.com>
2020-02-15 13:44:06 +0900

    fix AWS access

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'chatspace0518'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chatspace0518'
+  config.asset_host = 'https://s3.console.aws.amazon.com/s3/buckets/chatspace0518/?region=ap-northeast-1&tab=overview'
 end
