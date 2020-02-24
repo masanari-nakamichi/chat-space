0c6dc84bb84db5f4380fd2866f5e223ce5deae9f
masanari-nakamichi <shirohana2627@gmail.com>
2020-02-24 22:13:53 +0900

    Update unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -9,10 +9,10 @@ worker_processes 1
 working_directory "#{app_path}/current"
 
 #ポート番号を指定
-listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
+listen "/var/run/unicorn.sock"
 
 #Unicornの起動に必要なファイルの設置場所を指定
-pid "#{app_path}/shared/tmp/pids/unicorn.pid"
+pid "/var/run/unicorn.pid"
 
 
 #エラーのログを記録するファイルを指定
