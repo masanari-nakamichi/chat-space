986b4029ab71a6f77692a4cf268585df496f28ae
masanari-nakamichi <shirohana2627@gmail.com>
2020-02-28 18:52:19 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,6 +3,5 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 
-  config.fog_directory  = 'chatspace0518'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chatspace0518'
-end
+config.fog_directory  = 'chatspace0518'
+config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chatspace0518'
