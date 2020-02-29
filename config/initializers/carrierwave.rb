9ff3ea442b5b092dc4b2fb3f3c8be96ef6cc3245
masanari-nakamichi <shirohana2627@gmail.com>
2020-03-01 04:45:23 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
--require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
--CarrierWave.configure do |config|
+CarrierWave.configure do |config|
   config.storage = :fog
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
